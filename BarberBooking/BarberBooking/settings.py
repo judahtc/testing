e5882c1bb9209462c41b22a824092270b6267084
judahtc <84022158+judahtc@users.noreply.github.com>
2022-08-24 12:35:52 +0200

    states

--- BarberBooking/BarberBooking/settings.py
+++ BarberBooking/BarberBooking/settings.py
@@ -1,3 +1,6 @@
+
+import django_heroku
+import os
 """
 Django settings for BarberBooking project.
 
@@ -114,8 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+django_heroku.settings(locals())
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
